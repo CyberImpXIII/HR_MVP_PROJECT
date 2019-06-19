1a3ad56acf8f877cdef121135da2ba92e712cba8
Jacob <Specter@Jacobs-MacBook-Pro-4.local>
2019-06-19 17:35:48 -0500

    set up scraper for minimum data

--- client/app.js
+++ client/app.js
@@ -3,19 +3,29 @@ import Axios from "axios";
 
 const App = ()=>{
   const [ebaySearchResults, setEbaySearchResults] = useState({});
+  const [shopGResults,setShopG] = useState({});
   const [formInput, setFormInput] = useState('');
 
   const ebaySubmit = ()=>{
     const params = { formInput }
-    Axios.get('api')
+    Axios.get('api/ebay')
     .then(r=>setEbaySearchResults(r.data))
-    .catch((err)=>{'connection error "${}"'})
+    .catch((err)=>{'connection error "${err}"'})
+  }
+
+  const shopGSubmit = ()=>{
+    const params = { formInput }
+    Axios.get('api/shopg')
+    .then(r=>setShopG(r.data))
+    .catch((err)=>{'connection error "${err}"'})
   }
   
   return( 
   <div>
-  <button onClick={ebaySubmit}>Submit</button><br/>
-  {console.log(ebaySearchResults), JSON.stringify(ebaySearchResults)}
+  <button onClick={ebaySubmit}>Submit Ebay</button><br/>
+  <button onClick={shopGSubmit}>Submit ShopGoodwill</button><br/>
+  {console.log(ebaySearchResults)} {JSON.stringify(ebaySearchResults)}
+  {console.log(shopGResults)}
   </div>
   )
 }
--- server/server.js
+++ server/server.js
@@ -3,7 +3,10 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const app = express();
 const axios = require('axios')
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3009;
+const Nightmare = require('nightmare')
+const nightmare = Nightmare({ show: true })
+
 app.use(express.static("dist"));
 app.use(
   bodyParser.json({
@@ -11,25 +14,7 @@ app.use(
   })
 );
 
-app.get('/api', (req, res)=>{
-  const query = {
-    'OPERATION-NAME':'findCompletedItems',
-    'SERVICE-VERSION':'1.7.0',
-    'SECURITY-APPNAME':'JacobNel-shopGcom-PRD-051fd426b-cb604293',
-    'RESPONSE-DATA-FORMAT':'JSON',
-    'REST-PAYLOAD': null,
-    keywords:'Garmin+nuvi+1300+Automotive+GPS+Receiver',
-    categoryId:'156955',
-    'itemFilter(0).name':'Condition',
-    'itemFilter(0).value':'3000',
-    'itemFilter(1).name':'FreeShippingOnly',
-    'itemFilter(1).value':'true',
-    'itemFilter(2).name':'SoldItemsOnly',
-    'itemFilter(2).value':'true',
-    'sortOrder':'PricePlusShippingLowest',
-    'paginationInput.entriesPerPage':'2'
-  }
-  console.log('boop')
+app.get('/api/ebay', (req, res)=>{
   axios.get(`https://svcs.ebay.com/services/search/FindingService/v1?` +
   `OPERATION-NAME=findCompletedItems&` +
   `SERVICE-VERSION=1.7.0&` +
@@ -50,6 +35,36 @@ app.get('/api', (req, res)=>{
   .then(r=>res.send(r.data))
   .catch((err)=>{console.log(err), res.end('error, see server logs for details')})
 })
+
+
+app.get('/api/shopg', (req, res)=>{
+  nightmare
+  .goto('https://shopgoodwill.com')
+  .insert('#search-text-header', 'garmin')
+  .type('#search-text-header', '\u000d')
+  .wait('.page-options')
+  .evaluate(() => {
+    return Array.from(document.querySelectorAll('.product')).map(
+      productNode=>{
+        return(
+          {
+            link:productNode.href,
+            prodId: productNode.href.split('/Item/')[1],
+            Title: productNode.children[1].children[0].children[1].innerHTML.split('\n')[1].trim(),
+            CurrentPrice: productNode.children[1].children[1].children[0].innerHTML.split('\"')[0].split("<br>")[0].trim(),
+            buyItNow: productNode.children[0].children[1].innerHTML.split('span')[1]===">Buy It Now</" ? true : false
+          }
+        );
+      }
+    );
+  })
+  .end(r=>res.send(r))
+  .catch(error => {
+    console.error('Search failed:', error)
+    res.end()
+  })
+});
+
 
 app.listen(port, () => {
   console.log(`Sever is running on ${port}`);
