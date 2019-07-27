f5761b86117b60a7ec46bd72ee557144d27cf22d
Jacob <Specter@Jacobs-MacBook-Pro-4.local>
2019-07-27 10:55:43 -0400

    found a bunch of changes in the local directory that I never committed. uploading now

--- client/app.js
+++ client/app.js
@@ -1,59 +1,34 @@
 import React,{useEffect, useState} from "react";
 import Axios from "axios";
-import {LineGraph, graphEffect} from './components/LineGraph.jsx'
-import Style from './style.css'
+import Chart from 'chart.js';
+import './style.css';
+import moment from 'moment'
 
 const App = ()=>{
   const [ebaySearchResults, setEbaySearchResults] = useState({});
-  const [ebayGraphData, setEbayGraphData] = useState({values:[], dates:[]});
+  const [ebayGraphData, setEbayGraphData] = useState([]);
   const [goodwillGraphData, setGoodwillGraphData] = useState({values:[], dates:[]});
   const [goodwillResults,setGoodwillResults] = useState({});
   const [formInput, setFormInput] = useState('');
-  const [chart, setChart] = useState({})
-
-  useEffect(()=>{
-    ebaySearchResults.findCompletedItemsResponse ? setEbayGraphData(ebaySearchResults.findCompletedItemsResponse[0].searchResult[0].item.map(
-      (e)=>{
-        return({
-          title:e.title[0],
-          itemId:e.itemId[0],
-          soldPrice:e.sellingStatus[0].currentPrice[0].__value__,
-          soldDate:e.listingInfo[0].endTime[0]
-        })
-      })
-    ) : null;
-  },[ebaySearchResults])
-
-  useEffect(()=>{
-    goodwillResults[0] ? setGoodwillGraphData(goodwillResults.map(
-      (e)=>{
-        return({
-          title:e.Title,
-          itemId:e.prodId,
-          soldPrice:e.CurrentPrice,
-          soldDate:e.dateSold
-        })
-      })
-    ) : null;
-  },[goodwillResults])
-
+  const [chart, setChart] = useState([])
+  const [dates, setDates] = useState([])
 
   useEffect(()=>{
     const ctx = document.getElementById('priceChart').getContext('2d');
     const priceChart = new Chart(ctx, {
     type: 'line',
     data: {
-      labels: ebayGraphData.soldDate,
+      labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', "Aug", 'Sep', 'Oct', 'Nov', 'Dec'],
         datasets: [{
             label: 'Ebay Price Average Price Over Time',
-            data: ebayGraphData.soldPrice,
+            data: [0],
             borderColor: [
                 '#33FF00'
             ],
             borderWidth: 1
         },{
           label: 'Shop Goodwill Average Price Over Time',
-          data: [3,2,3,6,19,12],
+          data: [0],
           borderColor: [
             '#FF6600'
         ],
@@ -78,29 +53,74 @@ const App = ()=>{
 },[])
 
 useEffect(()=>{
-  ebayGraphData[0] ? 
-  (ebayGraphData.forEach(e=>chart.data.labels.push(e.soldDate)),
-  ebayGraphData.forEach(e=>chart.data.datasets[0].data.push(e.soldPrice)),
-  chart.update()) : null 
-},[ebayGraphData])
+  //sets the graph data based on the search results. I should migrate this to the server side later
+  if(ebaySearchResults.findCompletedItemsResponse && ebaySearchResults.findCompletedItemsResponse[0].searchResult[0]["@count"]!=="0"){
+    console.log(ebaySearchResults.findCompletedItemsResponse)
+    let output = {};
+    //below is a function that properly assigns the graph data but only if JS hasnt forced the large arrays to be arrays of arrays
+    ebaySearchResults.findCompletedItemsResponse[0].searchResult[0].item.forEach((e,i)=>{
+      output[moment(e.listingInfo[0].endTime[0])]=e.sellingStatus[0].currentPrice[0].__value__
+    })
+    //this was temporarily scrapped in order to have MVP this is the general direction that I would LIKE 
+    // ebaySearchResults.map(
+    //   (e)=>{
+    //     return e.map((ie)=>{
+    //       console.log(ie)
+    //         output[moment(ie.listingInfo[0].endTime[0])]=ie.sellingStatus[0].currentPrice[0].__value__
+    //       })
+    //   })
+    console.log(output, "output - ebay search results -> ebay graph data");
+    setEbayGraphData(output);
+  }
+},[ebaySearchResults])
 
 useEffect(()=>{
-  goodwillGraphData[0] ? 
-  (goodwillGraphData.forEach(e=>chart.data.labels.push(e.soldDate)),
-  goodwillGraphData.forEach(e=>chart.data.datasets[1].data.push(e.soldPrice)),
+      //sets the graph data based on the search results. I should migrate this to the server side later
+  if(goodwillResults[0]){
+    const arr = goodwillResults.map(
+      (e)=>{
+        return({
+          [e.dateSold]:parseFloat(e.CurrentPrice.substring(1))
+        })
+      }
+    )
+    let output = {exists:true};
+    arr.forEach(e=>{output={...output,...e}})
+    setGoodwillGraphData(output);
+  }
+},[goodwillResults])
+
+useEffect(()=>{
+  const dates = Object.keys({...ebayGraphData, ...goodwillGraphData})
+  delete dates.exists
+  setDates(dates);
+},[ebayGraphData, goodwillGraphData])
+
+useEffect(()=>{
+  Object.keys(ebayGraphData)[0]? 
+  (chart.data.datasets[0].data=[],
+  chart.data.labels = (dates),
+  chart.data.datasets[0].data = dates.map(d=>ebayGraphData[d]),
   chart.update()) : null 
-},[goodwillGraphData])
+},[ebayGraphData,dates])
+
+useEffect(()=>{
+  goodwillGraphData.exists ? 
+  ( 
+    chart.data.labels = dates,
+  chart.data.datasets[1].data = dates.map(d=>goodwillGraphData[d]),
+  chart.update()) : null 
+},[goodwillGraphData, dates])
 
   const ebaySubmit = ()=>{
-    const params = { formInput }
-    Axios.get('api/ebay')
+    Axios.get('api/ebay', {params : { formInput:formInput }})
     .then(r=>setEbaySearchResults(r.data))
     .catch((err)=>{'connection error "${err}"'})
   }
 
   const shopGSubmit = ()=>{
-    const params = { formInput }
-    Axios.get('api/shopg')
+    console.log(formInput);
+    Axios.get('api/shopg', {params : { formInput:formInput }})
     .then(r=>setGoodwillResults(r.data))
     .catch((err)=>{'connection error "${err}"'})
   }
@@ -114,8 +134,6 @@ useEffect(()=>{
     <input onChange={inputHandle}></input><br/>
     <button onClick={ebaySubmit}>Submit Ebay</button><br/>
     <button onClick={shopGSubmit}>Submit ShopGoodwill</button><br/>
-    {console.log(ebaySearchResults)} 
-    {console.log(goodwillResults)} 
     <div id='canvas-container'>
     <canvas id="priceChart" data-ebay={ebayGraphData.soldDate} width="800" height="800"></canvas>
     </div>
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 const axios = require('axios')
 const port = process.env.PORT || 3009;
 const Nightmare = require('nightmare')
-const nightmare = Nightmare({ show: true })
+const { exec } = require('child_process');
 
 app.use(express.static("dist"));
 app.use(
@@ -16,54 +16,130 @@ app.use(
 
 app.get('/api/ebay', (req, res)=>{
   // console.log(req.params.)
+  let responseArr = [];
   axios.get(`https://svcs.ebay.com/services/search/FindingService/v1?` +
   `OPERATION-NAME=findCompletedItems&` +
   `SERVICE-VERSION=1.7.0&` +
   `SECURITY-APPNAME=JacobNel-shopGcom-PRD-051fd426b-cb604293&` +
   `RESPONSE-DATA-FORMAT=JSON&` +
   `REST-PAYLOAD&` +
-  `keywords=Garmin&` +
+  `keywords=${req.query.formInput}&` +
   `categoryId=156955&` +
   `itemFilter(0).name=Condition&` +
   `itemFilter(0).value=3000&` +
   `itemFilter(1).name=SoldItemsOnly&` +
   `itemFilter(1).value=true&` +
   `sortOrder=PricePlusShippingLowest&` +
-  `paginationInput.entriesPerPage=100` 
-)
-  .then(r=>res.send(r.data))
+  `paginationInput.entriesPerPage=100&` +
+  `paginationInput.pageNumber=${1}` 
+) 
+.then((r)=>{
+  res.send(r.data)
+  //this is a stopgap that returns one page because of issues about handling all of the data at once and concerns over using all of my requests
+})
+  // .then((r)=>{
+  //   let pageNumber = parseInt(r.data.findCompletedItemsResponse[0].paginationOutput[0].totalPages[0])
+  //   return Promise.all(Array(pageNumber).fill(null).map((e,i)=>{
+  //     return (axios.get(`https://svcs.ebay.com/services/search/FindingService/v1?` +
+  //       `OPERATION-NAME=findCompletedItems&` +
+  //       `SERVICE-VERSION=1.7.0&` +
+  //       `SECURITY-APPNAME=JacobNel-shopGcom-PRD-051fd426b-cb604293&` +
+  //       `RESPONSE-DATA-FORMAT=JSON&` +
+  //       `REST-PAYLOAD&` +
+  //       `keywords=Garmin&` +
+  //       `categoryId=156955&` +
+  //       `itemFilter(0).name=Condition&` +
+  //       `itemFilter(0).value=3000&` +
+  //       `itemFilter(1).name=SoldItemsOnly&` +
+  //       `itemFilter(1).value=true&` +
+  //       `sortOrder=PricePlusShippingLowest&` +
+  //       `paginationInput.entriesPerPage=100&` +
+  //       `paginationInput.pageNumber=${i+1}` 
+  //       )
+  //     )
+  //   }))
+  // })
+  // .then(r=>{
+  //   // console.log(r[0].data.findCompletedItemsResponse[0].errorMessage[0].error[0]);
+  //   return responseArr = [...responseArr, ...r.map((e)=>{return e.data.findCompletedItemsResponse[0].searchResult[0].item})]
+  // })
+  // .then(r=>{console.log(responseArr),res.send(responseArr)})
   .catch((err)=>{console.log(err), res.end('error, see server logs for details')})
 })
 
 
 app.get('/api/shopg', (req, res)=>{
-  nightmare
-  .goto('https://www.shopgoodwill.com/Listings?st=garmin&sg=&c=&s=&lp=0&hp=999999&sbn=false&spo=false&snpo=false&socs=false&sd=false&sca=true&caed=6/19/2019&cadb=200&scs=false&sis=false&col=0&p=1&ps=40&desc=false&ss=0&UseBuyerPrefs=true')
-  .wait('.page-options')
-  .evaluate(() => {
-    return Array.from(document.querySelectorAll('.product')).map(
-      productNode=>{
-        return(
-          {
-            link:productNode.href,
-            prodId: productNode.href.split('/Item/')[1],
-            Title: productNode.children[1].children[0].children[1].innerHTML.split('\n')[1].trim(),
-            CurrentPrice: productNode.children[1].children[1].children[0].innerHTML.split('\"')[0].split("<br>")[0].trim(),
-            buyItNow: productNode.children[0].children[1].innerHTML.split('span')[1]===">Buy It Now</" ? true : false,
-            dateSold: productNode.children[1].children[1].innerHTML.split('data-countdown="')[1].split('"')[0],
-          }
-        );
-      }
-    );
+  console.log(req.query.formInput)
+  console.log('we made it inside!')
+    let pageCount = 0;
+
+  Nightmare({ show: false })
+  .goto(`https://www.shopgoodwill.com/Listings?st=${req.query.formInput}&sg=&c=&s=&lp=0&hp=999999&sbn=false&spo=false&snpo=false&socs=false&sd=false&sca=true&caed=6/19/2019&cadb=200&scs=false&sis=false&col=0&p=1&ps=40&desc=false&ss=0&UseBuyerPrefs=true`)
+  .wait('#last')
+  .evaluate(()=>{
+    return document.querySelector('#last').parentElement.innerHTML.split('data-page="')[1].split('"')[0]
   })
-  .end(r=>res.send(r))
-  .catch(error => {
-    console.error('Search failed:', error)
-    res.end()
+  .end((r)=>{
+    pageCount = r;
+    console.log(r, "number of pages");
+  })
+  .catch(console.log)
+  .then(async()=>{
+    console.log('trying to promise all');
+    // Promise.all(([...Array(pageCount)].map((e,i)=>{
+    // })))
+    await looperize(pageCount, req.query.formInput)
+    .then((responseArr)=>{
+      console.log('finished mapping', responseArr)
+      exec('killall electron');
+      res.send(responseArr);
+    })
+    .catch(error => {
+      console.error('Search failed:', error)
+      exec('killall electron');
+      res.end()
+    });
   })
 });
-
 
 app.listen(port, () => {
   console.log(`Sever is running on ${port}`);
 });
+
+
+const looperize = async(pageCount, query)=>{
+  let responseArr = [];
+  //This loop was changed to 10 because I think having it be the proper page count is causing it to time out and I need to find a solution
+  for(i=0; i < 2; i++){
+    console.log('scraping page', i)
+    await pagescrape(i, query)
+    .then((r)=>{responseArr = [...responseArr, ...r]})
+  }
+  console.log(responseArr);
+  return responseArr;
+}
+
+const pagescrape = (i, query)=>{
+   return Nightmare({ show: false })
+      .goto(`https://www.shopgoodwill.com/Listings?st=${query}&sg=&c=&s=&lp=0&hp=999999&sbn=false&spo=false&snpo=false&socs=false&sd=false&sca=true&caed=6/19/2019&cadb=200&scs=false&sis=false&col=0&p=${i+1}&ps=40&desc=false&ss=0&UseBuyerPrefs=true`)
+      .wait('.page-options')
+      .evaluate(() => {
+        return Array.from(document.querySelectorAll('.product')).map(
+          productNode=>{
+            return(
+              {
+                link:productNode.href,
+                prodId: productNode.href.split('/Item/')[1],
+                Title: productNode.children[1].children[0].children[1].innerHTML.split('\n')[1].trim(),
+                CurrentPrice: productNode.children[1].children[1].children[0].innerHTML.split('\"')[0].split("<br>")[0].trim(),
+                // buyItNow: productNode.children[0].children[1].innerHTML.split('span')[1]===">Buy It Now</" ? true : false,
+                dateSold: productNode.children[1].children[1].innerHTML.split('data-countdown="')[1].split('"')[0],
+              }
+            );
+          }
+        );
+      })
+      .end(r=>{
+        return (r)
+     })
+    }
